722eff493240609254c405d5d8f814ccf8beb3c2
rajadevops2025 <rajadevops2025@gmail.com>
2025-03-19 11:28:47 +0530

    Update AppTest.java

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,1 +1,12 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class AppTest {
+    @Test
+    void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
 
